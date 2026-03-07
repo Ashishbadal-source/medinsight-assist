d2b07349a92742113688d4982663c55dc226146a
ASHISH BADAL <idh4816@gmail.com>
2026-03-07 10:30:30 +0530

    Prepare_input_update

--- ecg_pipeline/ml/prepare_input.py
+++ ecg_pipeline/ml/prepare_input.py
@@ -12,12 +12,21 @@
 #     return x
 
 
+# import numpy as np
+
+# def prepare_ml_input(ecg):
+#     """
+#     ecg: numpy (5000, 12)
+#     returns: numpy (1, 5000, 12)
+#     """
+#     x = np.expand_dims(ecg, axis=0)  # (1, 5000, 12)
+#     return x
+
+
+
+
 import numpy as np
 
 def prepare_ml_input(ecg):
-    """
-    ecg: numpy (5000, 12)
-    returns: numpy (1, 5000, 12)
-    """
-    x = np.expand_dims(ecg, axis=0)  # (1, 5000, 12)
-    return x
+    # ecg: (5000, 12) → returns (1, 5000, 12)
+    return np.expand_dims(ecg, axis=0)
